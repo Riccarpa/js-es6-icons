9c855609717fa6c2f28f523a7bf9c4fa4bfcbc61
Riccarpa <riccardopagni99@gmail.com>
2021-07-28 15:04:05 +0200

    bonus 1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 /*
-
-Milestone 3
-Creiamo una select con i tipi di icone e usiamola per filtrare le icone
-:avviso:IMPORTANTE: non è obbligatorio usare la sintassi ES6, se non vi sentite a vostro agio partite dalla sintassi 
-con cui vi trovate meglio e cercate di migliorarla in un secondo momento.
 BONUS: Provare ad aggiungere una casella di testo che renda possibile filtrare anche per nome dell'icona
 SUPER INCREDIBLE MEGA BONUS: effettuare la ricerca dalla casella di testo in "tempo reale", cioè man mano che l'utente digita.
 
@@ -37,6 +32,9 @@ function printCards(iconsArray) {
 
 
 
+
+
+
 // esercizio************************************************
 
 
@@ -60,5 +58,22 @@ selectLink.addEventListener('change', () => {
     const filteredCards = icons.filter((item) => item.type === cardValue);
     return printCards(filteredCards);
 
+
+})
+
+
+
+
+const search = document.getElementById('search-by-word');
+
+
+
+search.addEventListener('change', () => {
+
+    let searchValue = search.value;
+
+    const filteredCards = icons.filter((item) => item.name === searchValue);
+
+    printCards(filteredCards);
 
 })
